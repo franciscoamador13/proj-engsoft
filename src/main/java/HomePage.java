de8e589e7fa5f5bbabbe0026d21038cb5817d73b
Pedro Pedro <pedro.afonso.pedro.pp@gmail.com>
2025-04-24 10:43:13 +0100

    Home and Menu Page

---
+++ src/main/java/HomePage.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HomePage extends JFrame {
+    private JPanel homePage;
+    private JButton startButton;
+
+    public HomePage(String title) {
+        super(title);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(homePage);
+        pack();
+
+        startButton.addActionListener(this::startButtonActionPerformed);
+
+    }
+
+    private void startButtonActionPerformed(ActionEvent e) {
+        setContentPane(new MenuPage().getContentPane());
+        revalidate();
+        repaint();
+    }
+
+    public static void main(String[] args) {
+        new HomePage("Cinema Management App").setVisible(true);
+    }
+}
---
+++ src/main/java/MenuPage.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class MenuPage extends JFrame {
+
+    private JPanel menuPage;
+    private JButton salasButton;
+    private JButton sessoesButton;
+    private JButton vendasButton;
+    private JButton barButton;
+    private JButton statsButton;
+
+    public MenuPage() {
+        setContentPane(menuPage);
+    }
+}
