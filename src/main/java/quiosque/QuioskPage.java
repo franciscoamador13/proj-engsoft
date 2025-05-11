2e572d8fdcaeca67f4e10ab76ab94fc1a9904cc3
Francisco Amador <franciscoamador13@outlook.pt>
2025-05-11 22:17:45 +0100

    added a button for choosing the seat

--- src/main/java/quiosque/QuioskPage.java
+++ src/main/java/quiosque/QuioskPage.java
@@ -13,6 +13,7 @@ public class QuioskPage extends JFrame {
     private JList list1;
     private JTextField textField1;
     private JTextField textField2;
+    private JButton escolherLugarButton;
 
     public QuioskPage() {
         super("Página quiosque");
