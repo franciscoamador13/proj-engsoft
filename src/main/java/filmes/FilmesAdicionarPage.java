30b4a1eebfa832c7442f98d40b86ff0e2a30fcb6
Pedro Pedro <pedro.afonso.pedro.pp@gmail.com>
2025-05-12 23:10:53 +0100

    filmes fix

--- src/main/java/filmes/FilmesAdicionarPage.java
+++ src/main/java/filmes/FilmesAdicionarPage.java
@@ -5,6 +5,10 @@ import javax.swing.*;
 public class FilmesAdicionarPage extends JFrame {
     private JPanel filmesAdicionarPage;
     private JButton adicionarButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
 
     public FilmesAdicionarPage() {
         super("Adicionar Filme");
