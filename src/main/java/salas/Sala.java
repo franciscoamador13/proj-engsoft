c5055136a610ae6a15cacf0dea5edaa9a3f9821e
Francisco Amador <franciscoamador13@outlook.pt>
2025-06-19 01:13:13 +0100

    AdicionarNovaSala e EditarSala implementados sem Singleton

---
+++ src/main/java/salas/Sala.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sala {
+    private String nomeSala;
+    private String nivelProjecao;
+    private String nivelSom;
+    private int linhas;
+    private int colunas;
+    private boolean acessivelCadeirantes;
+    private List<String> lugaresAcessiveis;
+
+    public Sala(String nomeSala, String nivelProjecao, String nivelSom, int linhas, int colunas, boolean acessivelCadeirantes) {
+        this.nomeSala = nomeSala;
+        this.nivelProjecao = nivelProjecao;
+        this.nivelSom = nivelSom;
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.acessivelCadeirantes = acessivelCadeirantes;
+        this.lugaresAcessiveis = new ArrayList<>();
+    }
+
+    public String getNomeSala() {
+        return nomeSala;
+    }
+
+    public String getNivelProjecao() {
+        return nivelProjecao;
+    }
+
+    public String getNivelSom() {
+        return nivelSom;
+    }
+
+    public int getLinhas() {
+        return linhas;
+    }
+
+    public int getColunas() {
+        return colunas;
+    }
+
+    public boolean isAcessivelCadeirantes() {
+        return acessivelCadeirantes;
+    }
+
+    public List<String> getLugaresAcessiveis() {
+        return lugaresAcessiveis;
+    }
+
+    public void setNomeSala(String nomeSala) {
+        this.nomeSala = nomeSala;
+    }
+
+    public void setNivelProjecao(String nivelProjecao) {
+        this.nivelProjecao = nivelProjecao;
+    }
+
+    public void setNivelSom(String nivelSom) {
+        this.nivelSom = nivelSom;
+    }
+
+    public void setLinhas(int linhas) {
+        this.linhas = linhas;
+    }
+
+    public void setColunas(int colunas) {
+        this.colunas = colunas;
+    }
+
+    public void setAcessivelCadeirantes(boolean acessivelCadeirantes) {
+        this.acessivelCadeirantes = acessivelCadeirantes;
+    }
+
+    // Método toString para exibição no JList
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(nomeSala)
+                .append(" - ").append(nivelProjecao)
+                .append(" (").append(linhas).append("x").append(colunas).append(")");
+
+        if (acessivelCadeirantes) {
+            sb.append(" [Acessível: ").append(lugaresAcessiveis.size()).append(" lugares]");
+        }
+
+        return sb.toString();
+    }
+}
